9978a86cbdfa5301662cb157031ded8c65f4647d
Rodrigo Sales <90647707+RodrigooSales@users.noreply.github.com>
2022-08-01 14:42:03 -0300

    Update Board.java

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,30 @@
 package boardgame;
 
 public class Board {
-    
+    private int row;
+    private int columns;
+    private Piece[][] pieces;
+
+
+    public Board(int row, int columns) {
+        this.row = row;
+        this.columns = columns;
+        pieces = new Piece[row][columns];
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public Piece piece(int row, int columns){
+        return pieces[row][columns];
+    }
+
+    public Piece piece(Position position){
+        return pieces[position.getRow()][position.getColumn()];
+    }
 }
