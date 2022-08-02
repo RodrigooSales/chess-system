c7096d62bdf69effefac655c4be53440f5a9a3ec
Rodrigo Sales <90647707+RodrigooSales@users.noreply.github.com>
2022-08-02 17:51:19 -0300

    Correção erro do checkmate
    mudança na ordem na classe testCheckMate capturedPiece troca de lugar com o testCheck

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -151,9 +151,9 @@ public class ChessMatch {
 
     private boolean testCheck(Color color) {
         Position kingPosition = king(color).getChessPosition().toPosition();
-        List<Piece> oponentPieces = piecesOnTheBoard.stream()
-                .filter(x -> ((ChessPiece) x).getColor() == opponent(color)).collect(Collectors.toList());
-        for (Piece p : oponentPieces) {
+        List<Piece> opponentPieces  = piecesOnTheBoard.stream()
+                .filter(x -> ((ChessPiece)x).getColor() == opponent(color)).collect(Collectors.toList());
+        for (Piece p : opponentPieces ) {
             boolean[][] mat = p.possibleMoves();
             if (mat[kingPosition.getRow()][kingPosition.getColumn()]) {
                 return true;
@@ -166,18 +166,17 @@ public class ChessMatch {
         if (!testCheck(color)) {
             return false;
         }
-
-        List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece) x).getColor() == color)
+        List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == color)
                 .collect(Collectors.toList());
         for (Piece p : list) {
             boolean[][] mat = p.possibleMoves();
             for (int i = 0; i < board.getRows(); i++) {
                 for (int j = 0; j < board.getColumns(); j++) {
                     if (mat[i][j]) {
-                        Position source = ((ChessPiece) p).getChessPosition().toPosition();
+                        Position source = ((ChessPiece)p).getChessPosition().toPosition();
                         Position target = new Position(i, j);
-                        boolean testCheck = testCheck(color);
                         Piece capturedPiece = makeMove(source, target);
+                        boolean testCheck = testCheck(color);
                         undoMove(source, target, capturedPiece);
                         if (!testCheck) {
                             return false;
